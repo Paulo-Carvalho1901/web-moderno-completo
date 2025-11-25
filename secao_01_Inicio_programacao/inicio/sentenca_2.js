ad994a58dc67ce38c33d40372578333f94451f82
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-11-24 23:08:03 -0300

    feat: a ordem das linhas  importa

--- secao_01_Inicio_programacao/inicio/sentenca_2.js
+++ secao_01_Inicio_programacao/inicio/sentenca_2.js
@@ -1,3 +1,8 @@
 // Exibindo algo na tela
 console.log('Hello, World!'); // Texto str
 console.log(1 + 3); // Soma dois número inteiro
+
+// A ordem das linhas importan
+console.log('Passo #1');
+console.log('Passo #2');
+console.log('Passo #3');
